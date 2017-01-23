dfc74eaa191619ff317752a8c6ba755514144e7d
kuwako <m.kuwako0702@gmail.com>
2017-01-23 10:14:46 +0000

    expressでサーバー起動

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    app = express();
+
+app.get('/', function(req, res) {
+    res.send("hello world");
+});
+
+app.listen(3000);
+console.log("server running");
+
