934629e43ab4ea543d6db45380762e5b62db0cbc
kuwako <m.kuwako0702@gmail.com>
2017-01-24 09:42:17 +0000

    routing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express'),
     app = express();
 
-app.get('/', function(req, res) {
-    res.send("hello world");
+app.get('/users/:name', function(req, res) {
+    res.send('hello, ' + req.params.name);
+});
+
+app.get('/items/:id([0-9]+)', function(req, res) {
+    res.send('item no.' + req.params.id);
 });
 
 app.listen(3000);
