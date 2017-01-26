3f33ce53644456e9bc42948ab2bbe97871349d69
kuwako <m.kuwako0702@gmail.com>
2017-01-26 10:28:44 +0000

    app.paramの使用

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/public'));
+
+app.param('id', function(req, res, next, id){
+    var users = ['kuwako', 'mask', 'MasK'];
+    req.params.name = users[id];
+    next();
+});
+app.get('/hello/:id', function(req, res) {
+    res.send('hello ' + req.params.name);
+});
+
 app.get('/', function(req, res) {
     res.render('index', {title: "TITLE"});
 });
