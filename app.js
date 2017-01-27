be22089beaa61e54b6d6eb2d83a72c21e11b0810
kuwako <m.kuwako0702@gmail.com>
2017-01-27 09:51:01 +0000

    bodyParserでPOSTを受け取る

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 var express = require('express'),
     app = express(),
-    logger = require('morgan');
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 // middleware
-app.use(logger('dev'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
-app.param('id', function(req, res, next, id){
-    var users = ['kuwako', 'mask', 'MasK'];
-    req.params.name = users[id];
-    next();
-});
-app.get('/hello/:id', function(req, res) {
-    res.send('hello ' + req.params.name);
+app.get('/new', function(req, res) {
+    res.render('new');
 });
 
-app.get('/', function(req, res) {
-    res.render('index', {title: "TITLE"});
+app.post('/create', function(req, res) {
+    res.send(req.body.name);
 });
 
 app.listen(3000);
