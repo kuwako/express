8e945051a69abac6825d7f898e73f80f9f0fe8b3
kuwako <m.kuwako0702@gmail.com>
2017-01-31 11:08:22 +0000

    routingを別ファイルで処理

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    post = require('./routes/post');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -12,14 +13,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/new', function(req, res) {
-    res.render('new');
-});
-
-app.post('/create', function(req, res) {
-    res.send(req.body.name);
-});
-
+// routing
+app.get('/', post.index);
+/*
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id/', post.update);
+app.delete('/posts/:id/', post.destroy);
+*/
 app.listen(3000);
 console.log("server running");
 
